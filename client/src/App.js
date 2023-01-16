b1b456d6c5880de526bbb3797f0dba40527917d1
Erik Trejo Lujano <85711028+eTrejoLujano@users.noreply.github.com>
2023-01-15 17:56:26 -0800

    package json set up

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ProjectLists from "./components/ProjectLists";
+
+function App() {
+  return (
+    <div className="App">
+      <ProjectLists />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ProjectLists.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectLists = () => {
+  return (
+    <div>
+      <h1>ProjectLists</h1>
+    </div>
+  );
+};
+
+export default ProjectLists;
